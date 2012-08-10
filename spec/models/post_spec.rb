39acd8d2bc644d6435406d3f63f3fd7541f1c3b8
Hans Hasselberg <me@hans.io>
2012-08-10 09:42:06 +0200

    Correct specs to show, that there is no problem with mongoid.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,12 +13,8 @@ describe Post do
     end
 
     it "should saves ids on both sides (relation by ids)" do
-      tag  = Tag.create(name: "tag1")
-      post = Post.new(title: "post", tag_ids: [tag.id])
-      post.save
-      
-      post.reload
-      tag.reload
+      tag  = Tag.create!(name: "tag1")
+      post = Post.create!(title: "post", tags: [tag])
 
       post.tag_ids.should eq([tag.id])
       tag.post_ids.should eq([post.id])
